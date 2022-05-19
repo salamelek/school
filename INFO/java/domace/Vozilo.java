dd9911ca57fbd1afd06f65a7a37ea9c877595d87
salamelek <samuel.sluga@gmail.com>
2022-05-19 19:27:57 +0200

    popravil Vozilo.java

--- INFO/java/domace/Vozilo.java
+++ INFO/java/domace/Vozilo.java
@@ -6,18 +6,10 @@ public class Vozilo {
     private final double PORABA = 7.4;
     private final double MAXLIT = 55;
 
-    public Vozilo() {
+    public Vozilo(double krmilo) {
         this.gorivo = 0;
-        this. krmilo = 0;
+        this.krmilo = krmilo;
         this.motor = false;
         this.pot = 0;
     }
-
-    public Vozilo(float gorivo, double krmilo, boolean motor, double pot) {
-        this.gorivo = gorivo;
-        this. krmilo = krmilo;
-        this.motor = motor;
-        this.pot = pot;
-    }
-
 }
