7d273331c175299192a707fb5f7a6818f654ca67
salamelek <samuel.sluga@gmail.com>
2022-05-31 18:34:23 +0200

    DN_4Z_20220530

--- INFO/java/domace/DN_4Z_20220530/src/Avto.java
+++ INFO/java/domace/DN_4Z_20220530/src/Avto.java
@@ -1,3 +1,113 @@
 public class Avto extends Vozilo{
+    private boolean spLevaVrata;
+    private boolean spDesnaVrata;
+    private boolean zaLevaVrata;
+    private boolean zaDesnaVrata;
+    private boolean prtljaznik;
 
+    protected Avto(float gorivo, double krmilo, boolean motor, double pot, double poraba, double maxLit) {
+        super(gorivo, krmilo, motor, pot, poraba, maxLit);
+    }
+
+    public void odpriSpLevaVrata() {
+        if (!this.spLevaVrata) {
+            this.spLevaVrata = true;
+            System.out.println("Sprednja leva vrata so sedaj odprta.");
+        } else {
+            System.out.println("Sprednja leva vrata so že odprta.");
+        }
+    }
+
+    public void odpriSpDesnaVrata() {
+        if (!this.spDesnaVrata) {
+            this.spDesnaVrata = true;
+            System.out.println("Sprednja desna vrata so sedaj odprta.");
+        } else {
+            System.out.println("Sprednja desna vrata so že odprta.");
+        }
+    }
+
+    public void odpriZaLevaVrata() {
+        if (!this.zaLevaVrata) {
+            this.zaLevaVrata = true;
+            System.out.println("Zadnja leva vrata so sedaj odprta.");
+        } else {
+            System.out.println("Zadnja leva vrata so že odprta.");
+        }
+    }
+
+    public void odprZaDesnaVrata() {
+        if (!this.zaDesnaVrata) {
+            this.zaDesnaVrata = true;
+            System.out.println("Zadnja desna vrata so sedaj odprta.");
+        } else {
+            System.out.println("Zadnja desna vrata so že odprta.");
+        }
+    }
+
+    public void odpriPrtljaznik() {
+        if (!this.prtljaznik) {
+            this.prtljaznik = true;
+            System.out.println("Prtljažnik je sedaj odprt.");
+        } else {
+            System.out.println("Prtljažnik je že zaprt.");
+        }
+    }
+
+    public void zapriSpLevaVrata() {
+        if (this.spLevaVrata) {
+            this.spLevaVrata = false;
+            System.out.println("Sprednja leva vrata so sedaj zaprta.");
+        } else {
+            System.out.println("Sprednja leva vrata so že zaprta.");
+        }
+    }
+
+    public void zapriSpDesnaVrata() {
+        if (this.spDesnaVrata) {
+            this.spDesnaVrata = false;
+            System.out.println("Sprednja desna vrata so sedaj zaprta.");
+        } else {
+            System.out.println("Sprednja desna vrata so že zaprta.");
+        }
+    }
+
+    public void zapriZaLevaVrata() {
+        if (this.zaLevaVrata) {
+            this.zaLevaVrata = false;
+            System.out.println("Zadnja leva vrata so sedaj zaprta.");
+        } else {
+            System.out.println("Zadnja leva vrata so že zaprta.");
+        }
+    }
+
+    public void zapriZaDesnaVrata() {
+        if (this.zaDesnaVrata) {
+            this.zaDesnaVrata = false;
+            System.out.println("Zadnja desna vrata so sedaj zaprta.");
+        } else {
+            System.out.println("Zadnja desna vrata so že zaprta.");
+        }
+    }
+
+    public void zapriPrtljaznik() {
+        if (this.prtljaznik) {
+            this.prtljaznik = false;
+            System.out.println("Prtljažnik je sedaj zaprt.");
+        } else {
+            System.out.println("Prtljažnik je že zaprt.");
+        }
+    }
+
+    public void zapriVsaVrata() {
+        zapriSpLevaVrata();
+        zapriSpDesnaVrata();
+        zapriZaLevaVrata();
+        zapriZaDesnaVrata();
+        zapriPrtljaznik();
+    }
+
+    public boolean aliSoVrataZaprta() {
+        return !(this.spLevaVrata || this.spDesnaVrata || this.zaLevaVrata || this.zaDesnaVrata || this.prtljaznik);
+    }
 }
--- INFO/java/domace/DN_4Z_20220530/src/Vozilo.java
+++ INFO/java/domace/DN_4Z_20220530/src/Vozilo.java
@@ -1,2 +1,69 @@
 public class Vozilo {
+    protected float gorivo;
+    protected double krmilo;
+    protected boolean motor;
+    protected double pot;
+    protected double poraba;
+    protected double maxLit;
+
+    protected Vozilo(float gorivo, double krmilo, boolean motor, double pot, double poraba, double maxLit) {
+        this.gorivo = gorivo;
+        this.krmilo = krmilo;
+        this.motor = motor;
+        this.pot = pot;
+        this.poraba = poraba;
+        this.maxLit = maxLit;
+    }
+
+    protected void prizgiMotor(){
+        if (!this.motor) {
+            this.motor = true;
+            System.out.println("Motor je sedaj prižgan!");
+        } else {
+            System.out.println("Motor je že prižgan!");
+        }
+    }
+
+    protected void ugasniMotor() {
+        if (this.motor) {
+            this.motor = false;
+            System.out.println("Motor je sedaj ugasnjen!");
+        } else {
+            System.out.println("Motor je že ugasnjen!");
+        }
+    }
+
+    protected void obrniKrmilo(double novoKrmilo) {
+        this.krmilo += novoKrmilo;
+        System.out.println("Krmilo je zdaj obrnjeno na " + this.krmilo + " stopinj.");
+    }
+
+    protected void napolniRezervoar(float kolicina) {
+        float novaKolicina = this.gorivo + kolicina;
+        if (novaKolicina <= this.maxLit) {
+            this.gorivo = novaKolicina;
+            System.out.println("Gorivo je bilo natočeno v rezervoar! trenutna količina: " + this.gorivo + "l.");
+        } else {
+            System.out.println("Taka količina goriva je prevelika za rezervoar vozila.");
+        }
+    }
+
+    protected void stanjeRezervoar() {
+        System.out.println("v rezervoarju je " + this.gorivo + " litrov goriva");
+    }
+
+    protected void prevozi(double prevoziPot) {
+        float potrebovanoGorivo = (float) (prevoziPot * (this.poraba / 10000));
+        if (!this.motor) {
+            System.out.println("Ni mogoče prevoziti poti, ker je motor ugasnjen!");
+            return;
+        }
+        if (this.gorivo >= potrebovanoGorivo) {
+            this.pot += prevoziPot;
+            this.gorivo -= potrebovanoGorivo;
+            System.out.println("Prevoženih je bilo " + prevoziPot + " metrov.");
+        } else {
+            System.out.println("Ni dovolj goriva, da bi prevozili toliko poti.");
+        }
+    }
 }
